4245b595747f6eb15aa18d0c388beefe0378eccc
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2021-12-23 22:49:46 -0300

    first commit

---
+++ desafio-clases.js
@@ -0,0 +1,8 @@
+class Usuario {
+    constructor (nombre, apellido, libros, mascotas)
+    {nombre: "carlos";
+    apellido:"monge";
+    libros:["patolucito","el principito"]
+    mascotas:["perros","gatos"]
+    }
+}
