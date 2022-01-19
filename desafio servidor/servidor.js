38fca4e8d3a7c18db74c8808ce2c444a08bddd66
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2022-01-19 11:14:15 -0300

    update

--- desafio servidor/servidor.js
+++ desafio servidor/servidor.js
@@ -5,7 +5,15 @@ const app = express()
 const PORT = 8080
 
 const fs = require("fs");
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
 class Contenedor {
   constructor(nombreArchivo) {
     this.nombreArchivo = nombreArchivo;
@@ -22,19 +30,21 @@ class Contenedor {
         } else {
           console.log(contenidoId);
         }
-        
+        app.get('/productoRandom', (req, res) => {
+          res.send( `Producto: ${contenidoId.title}`)
+         })
       } catch (err) {
         console.log("error", err);
       }
-      return contenidoId
+      
     }
     leerArchivo(this.nombreArchivo);
     
   }
   getAll() {
-    
+    let array = [];
     async function leerArchivo(nombreArchivo) {
-      let array = [];
+      
       try {
         const contenido = JSON.parse(
           await fs.promises.readFile(nombreArchivo, "utf-8")
@@ -42,36 +52,33 @@ class Contenedor {
         
         contenido.forEach((e) => array.push(e));
         
+        app.get('/productos', (req, res) => {
+           res.send(array.map( prod => `Producto${prod.id}: ${prod.title}`))
+
+         })
       } catch (err) {
         console.log("error", err);
       }
+     
       
-      return array
     }
     
    leerArchivo(this.nombreArchivo);
-    
-    
+   
+   
   }
+
   
   
 }
 
-function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-const Productos = new Contenedor("./productos.json");
 
-const server = app.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-})
-server.on("error", error => console.log(`Error en servidor ${error}`))
 app.get('/', (req, res) => {
   res.send('hola Bienvenido!' )
 })
-app.get('/productos', (req, res) => {
- res.send( `Productos: ${Productos.getAll()}`)
-})
-app.get('/productoRandom', (req, res) => {
-  res.send( `Producto: ${Productos.getById(getRandomArbitrary(1,3))}`)
- })
+
+
+
+ const Productos = new Contenedor("./productos.txt");
+ Productos.getAll()
+ Productos.getById(getRandomArbitrary(1,3))
